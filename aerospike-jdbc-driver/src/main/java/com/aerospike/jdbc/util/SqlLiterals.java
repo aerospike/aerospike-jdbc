b2d922a0778e9b76a7efd3c82d6880a65c123c86
yrizhkov <yrizhkov@aerospike.com>
2020-12-31 16:02:14 +0200

    clean out PredExp operators

--- aerospike-jdbc-driver/src/main/java/com/aerospike/jdbc/util/SqlLiterals.java
+++ aerospike-jdbc-driver/src/main/java/com/aerospike/jdbc/util/SqlLiterals.java
@@ -1,21 +1,14 @@
 package com.aerospike.jdbc.util;
 
-import com.aerospike.client.query.PredExp;
-import com.aerospike.jdbc.sql.type.BasicArray;
-
-import java.sql.Array;
 import java.sql.Types;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.TreeMap;
-import java.util.function.Supplier;
 
 import static java.sql.Types.NULL;
 import static java.sql.Types.OTHER;
-import static java.util.Arrays.asList;
-import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toMap;
 
 public class SqlLiterals {
@@ -84,45 +77,6 @@ public class SqlLiterals {
         sqlToJavaTypes.put(Types.DATE, java.sql.Date.class);
         sqlToJavaTypes.put(Types.TIME, java.sql.Time.class);
         sqlToJavaTypes.put(Types.TIMESTAMP, java.sql.Timestamp.class);
-    }
-
-    public static final Map<String, Supplier<PredExp>> predExpOperators = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
-
-    static {
-        predExpOperators.put(operatorKey(String.class, "="), PredExp::stringEqual);
-        predExpOperators.put(operatorKey(String.class, "<>"), PredExp::stringUnequal);
-        predExpOperators.put(operatorKey(String.class, "!="), PredExp::stringUnequal);
-        predExpOperators.put(operatorKey(String.class, "LIKE"), () -> PredExp.stringRegex(0));
-        predExpOperators.put(operatorKey(String.class, "AND"), () -> PredExp.and(2));
-        predExpOperators.put(operatorKey(String.class, "OR"), () -> PredExp.or(2));
-
-        for (Class<?> type : new Class[]{Byte.class, Short.class, Integer.class, Long.class}) {
-            predExpOperators.put(operatorKey(type, "="), PredExp::integerEqual);
-            predExpOperators.put(operatorKey(type, "<>"), PredExp::integerUnequal);
-            predExpOperators.put(operatorKey(type, "!="), PredExp::integerUnequal);
-            predExpOperators.put(operatorKey(type, ">"), PredExp::integerGreater);
-            predExpOperators.put(operatorKey(type, ">="), PredExp::integerGreaterEq);
-            predExpOperators.put(operatorKey(type, "<"), PredExp::integerLess);
-            predExpOperators.put(operatorKey(type, "<="), PredExp::integerLessEq);
-            predExpOperators.put(operatorKey(type, "AND"), () -> PredExp.and(2));
-            predExpOperators.put(operatorKey(type, "OR"), () -> PredExp.or(2));
-            predExpOperators.put(operatorKey(type, "IN"), () -> null);
-        }
-        for (Class<?> type : new Class[]{byte[].class, int[].class, long[].class, Byte[].class, Integer[].class,
-                Long[].class, Array.class, BasicArray.class}) {
-            predExpOperators.put(operatorKey(type, "IN"), () -> null);
-        }
-        predExpOperators.put(operatorKey(byte[].class, "="), () -> null);
-
-        // null is for prepared statement when type of value is unknown during parsing of expression.
-        asList("=", "<>", "!=", "<", "<=", ">", ">=", "LIKE").forEach(op ->
-                predExpOperators.put(operatorKey(null, op), () -> null));
-        predExpOperators.put(operatorKey(null, "AND"), () -> PredExp.and(2));
-        predExpOperators.put(operatorKey(null, "OR"), () -> PredExp.or(2));
-    }
-
-    public static String operatorKey(Class<?> type, String operand) {
-        return ofNullable(type).map(Class::getName).orElse(null) + operand;
     }
 
     public static int getSqlType(Object value) {
