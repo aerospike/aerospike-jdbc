669c931d608afd0442899ecb9edf357c1586f9fc
yrizhkov <yrizhkov@aerospike.com>
2025-11-12 16:27:19 +0200

    FMWK-854 Update dependencies and set version to 2.0.1

--- src/main/java/com/aerospike/jdbc/util/Constants.java
+++ src/main/java/com/aerospike/jdbc/util/Constants.java
@@ -12,7 +12,7 @@ public final class Constants {
     public static final String UNSUPPORTED_QUERY_TYPE_MESSAGE = "Unsupported query type";
 
     // Driver version
-    public static final String DRIVER_VERSION = "2.0.0";
+    public static final String DRIVER_VERSION = "2.0.1";
     public static final int DRIVER_MAJOR_VERSION = 2;
     public static final int DRIVER_MINOR_VERSION = 0;
 
